8fee631956d3818eadde47cb2301c04ff4539f0d
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-01-06 23:40:40 -0600

    set up function prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", prompt1);
+
+var length;
+var includeLowercase = false;
+var includeUppercase = false;
+var includeNumbers = false;
+var includeSpecialCharacters = false;
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+function prompt1() {
+  // "Choose password length: (8-128)" 
+  var criteria = window.prompt("choose password length: (8-128)")
+  //you'll need to convert string to number then check that its in range
+  length = criteria
+  prompt2()
+}
+
+function prompt2() {
+  //choose alternate criteria
+  var criteria = window.prompt('Do you want to include lowercase letters (yes/no)?')
+  if (criteria.toLowerCase() === "yes") {
+    includeLowercase = true
+  } else {
+    includeLowercase = false
+  }
+  
+  prompt3()
+}
+
+function prompt3() {
+  //choose alternate criteria
+  var criteria = window.prompt('Do you want to include uppercase letters (yes/no)?')
+  if (criteria.toLowerCase() === "yes") {
+    includeUppercase = true
+  } else {
+    includeUppercase = false
+  }
+  prompt4()
+}
+
+function prompt4() {
+  //choose alternate criteria
+  var criteria = window.prompt('Do you want to include numbers (yes/no)?')
+  if (criteria.toLowerCase() === "yes") {
+    includeNumbers = true
+  } else {
+    includeNumbers = false
+  }
+  prompt5()
+}
+
+function prompt5() {
+  //choose alternate criteria
+  var criteria = window.prompt('Do you want to include special characters (yes/no)?')
+  if (criteria.toLowerCase() === "yes") {
+    includeSpecialCharacters = true
+  } else {
+    includeSpecialCharacters = false
+  }
+  //check that one is true
+  writePassword()
+}
+
+// Generate password based on criteria
+function generatePassword() {
+  // return password logic here if else thing, switch statement? 
+  return "password"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +80,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
